09a42fb848e31c32c417d3a7733001a6f1761dfc
Yue Yin <yueyin.dev@gmail.com>
2022-09-18 15:58:38 -0400

    Array implementations

--- src/array/primitive_array.rs
+++ src/array/primitive_array.rs
@@ -1,4 +1,5 @@
 use super::{Array, ArrayBuilder};
+use bitvec::vec::BitVec;
 use std::fmt::Debug;
 
 /// A collection of primitive types, such as `i32`, `f32`.
@@ -13,7 +14,8 @@ impl Primitive for f64 {}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct PrimitiveArray<T: Primitive> {
-    // Add new fields here, if any
+    // is_valid.len() = # of items
+    is_valid: BitVec,
     data: Vec<T>,
 }
 
@@ -23,22 +25,25 @@ impl<T: Primitive> Array for PrimitiveArray<T> {
     type Item = T;
 
     fn get(&self, idx: usize) -> Option<&Self::Item> {
-        todo!()
+        if self.is_valid.len() <= idx || !self.is_valid[idx] {
+            None
+        } else {
+            Some(&self.data[idx])
+        }
     }
 
     fn len(&self) -> usize {
-        todo!()
+        self.is_valid.len()
     }
 
     fn is_empty(&self) -> bool {
         self.len() == 0
     }
-    // ...
 }
 
 /// A builder that constructs a [`PrimitiveArray`] from `Option<T>`.
 pub struct PrimitiveArrayBuilder<T: Primitive> {
-    // Add new fields here, if any
+    is_valid: BitVec,
     data: Vec<T>,
 }
 
@@ -46,18 +51,72 @@ impl<T: Primitive> ArrayBuilder for PrimitiveArrayBuilder<T> {
     type Array = PrimitiveArray<T>;
 
     fn with_capacity(capacity: usize) -> Self {
-        todo!()
+        Self {
+            is_valid: BitVec::with_capacity(capacity),
+            data: Vec::with_capacity(capacity),
+        }
     }
 
     fn push(&mut self, value: Option<&<Self::Array as Array>::Item>) {
-        todo!()
+        self.is_valid.push(value.is_some());
+        self.data.push(value.cloned().unwrap_or_default());
     }
 
     fn append(&mut self, other: &Self::Array) {
-        todo!()
+        self.is_valid.extend(&other.is_valid);
+        self.data.extend(&other.data);
     }
 
     fn finish(self) -> Self::Array {
-        todo!()
+        Self::Array {
+            is_valid: self.is_valid,
+            data: self.data,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push() {
+        let num_to_val = |x| if x % 2 == 0 { Some(x) } else { None };
+
+        let mut builder = PrimitiveArrayBuilder::with_capacity(0);
+        for v in 0..1000 {
+            builder.push(num_to_val(v).as_ref());
+        }
+        let arr = builder.finish();
+
+        assert_eq!(arr.len(), 1000);
+        for i in 0..1000 {
+            assert_eq!(num_to_val(i), arr.get(i as usize).cloned());
+        }
+    }
+
+    #[test]
+    fn test_append() {
+        let num_to_val = |x| if x % 2 == 0 { Some(x) } else { None };
+
+        // 0..499
+        let mut builder1 = PrimitiveArrayBuilder::with_capacity(500);
+        for v in 0..500 {
+            builder1.push(num_to_val(v).as_ref());
+        }
+        let arr1 = builder1.finish();
+
+        // 0..499 + 500..999
+        let mut builder2 = PrimitiveArrayBuilder::with_capacity(0);
+        builder2.append(&arr1);
+        for v in 500..1000 {
+            builder2.push(num_to_val(v).as_ref());
+        }
+        let arr2 = builder2.finish();
+
+        assert_eq!(arr2.len(), 1000);
+        for i in 0..1000 {
+            assert_eq!(num_to_val(i), arr2.get(i as usize).cloned());
+        }
     }
 }
--- src/array/utf8_array.rs
+++ src/array/utf8_array.rs
@@ -1,6 +1,11 @@
 use super::{Array, ArrayBuilder};
+use bitvec::vec::BitVec;
 
-pub struct Utf8Array {}
+pub struct Utf8Array {
+    is_valid: BitVec, // is_valid.len() = # of items
+    data: Vec<u8>,
+    offset: Vec<usize>, // offset[i], offset[i+1] is (start, end) of element i
+}
 
 impl Array for Utf8Array {
     type Builder = Utf8ArrayBuilder;
@@ -8,32 +13,84 @@ impl Array for Utf8Array {
     type Item = str;
 
     fn get(&self, idx: usize) -> Option<&Self::Item> {
-        todo!()
+        if self.is_valid.len() <= idx || !self.is_valid[idx] {
+            None
+        } else {
+            std::str::from_utf8(&self.data[self.offset[idx]..self.offset[idx + 1]]).ok()
+        }
     }
 
     fn len(&self) -> usize {
-        todo!()
+        return self.is_valid.len();
     }
 }
 
-pub struct Utf8ArrayBuilder {}
+pub struct Utf8ArrayBuilder {
+    is_valid: BitVec,
+    data: Vec<u8>,
+    offset: Vec<usize>,
+}
 
 impl ArrayBuilder for Utf8ArrayBuilder {
     type Array = Utf8Array;
 
     fn with_capacity(capacity: usize) -> Self {
-        todo!()
+        let mut offset = Vec::with_capacity(capacity + 1);
+        offset.push(0);
+        Self {
+            is_valid: BitVec::with_capacity(capacity),
+            data: Vec::with_capacity(capacity),
+            offset,
+        }
     }
 
     fn push(&mut self, value: Option<&<Self::Array as Array>::Item>) {
-        todo!()
+        self.is_valid.push(value.is_some());
+        if let Some(v) = value {
+            self.data.extend_from_slice(v.as_bytes());
+        }
+        self.offset.push(self.data.len());
     }
 
     fn append(&mut self, other: &Self::Array) {
-        todo!()
+        self.is_valid.extend(&other.is_valid);
+        self.data.extend(&other.data);
+
+        let curr_len = self.offset.len();
+        let curr_offset = self.offset.last().copied().unwrap();
+        self.offset.extend(&other.offset[1..]);
+        for i in 0..other.len() {
+            self.offset[curr_len + i] += curr_offset;
+        }
     }
 
     fn finish(self) -> Self::Array {
-        todo!()
+        Utf8Array {
+            is_valid: self.is_valid,
+            data: self.data,
+            offset: self.offset,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push() {
+        let vals = vec![None, Some("1"), None, Some("3")];
+
+        let mut builder = Utf8ArrayBuilder::with_capacity(0);
+        for v in vals {
+            builder.push(v);
+        }
+        let arr = builder.finish();
+
+        assert_eq!(arr.len(), 4);
+        assert_eq!(arr.get(0), None);
+        assert_eq!(arr.get(1), Some("1"));
+        assert_eq!(arr.get(2), None);
+        assert_eq!(arr.get(3), Some("3"));
     }
 }
